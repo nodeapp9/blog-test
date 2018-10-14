1a3ca7d5f248694419cecd5f8c19df5b7b9aba82
nodeapp911 <nodeapp911@gmail.com>
2018-10-14 19:32:10 +0545

    check2

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+//set home page
+var express = require('express');
+var router = express.Router();
+
+
+// Get Homepage
+router.get('/', function(req, res){
+	res.render('index');
+});
+
+module.exports = router;
